834c2cab2d84420fab9b0228482d915deb4c5df1
lixi1 <lixi1@LAPTOP-3NTHUUGV.station>
2018-09-09 15:39:12 +0200

    varie

--- webapp/src/main/java/com/primawebapp/webapp/controller/WelcomeController.java
+++ webapp/src/main/java/com/primawebapp/webapp/controller/WelcomeController.java
@@ -1,12 +1,10 @@
 package com.primawebapp.webapp.controller;
 
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WelcomeController {
@@ -14,13 +12,13 @@ public class WelcomeController {
 	@Value("${welcome.hello}")
 	public String welcomeHello;
 	
-	@Value("${cliccami.btn}")
-	public String cliccami;
+	@Value("${clicktocalculate.btn}")
+	public String clicktocalculate;
 	
 	@GetMapping("/")
 	public String welcome(Model model){
 		model.addAttribute("hello", welcomeHello);
-		model.addAttribute("cliccami", cliccami);
+		model.addAttribute("clicktocalculate", clicktocalculate);
 		return "welcome";
 	}
 }
